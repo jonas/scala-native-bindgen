23492af743eb8a77b274987df66739c825d31245
Jonas Fonseca <jonas.fonseca@gmail.com>
2018-07-22 22:54:59 -0400

    Remove unused sbt plugin setting

--- sbt-scala-native-bindgen/src/main/scala/org/scalanative/bindgen/sbt/ScalaNativeBindgenPlugin.scala
+++ sbt-scala-native-bindgen/src/main/scala/org/scalanative/bindgen/sbt/ScalaNativeBindgenPlugin.scala
@@ -120,7 +120,6 @@ object ScalaNativeBindgenPlugin extends AutoPlugin {
         },
         nativeBindgenPackage := None,
         nativeBindgenExclude := None,
-        resourceDirectories in nativeBindgen := resourceDirectories.value,
         sourceGenerators += Def.task { Seq(nativeBindgen.value) },
         name in nativeBindgen := "ScalaNativeBindgen",
         libraryDependencies ++= {
